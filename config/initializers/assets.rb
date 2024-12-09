d9086a4bd390388985a4a8589d4a503167777335
Randy Burgess <wrburgess@gmail.com>
2024-12-09 10:48:01 -0600

    updating dependencies

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,6 +6,3 @@ Rails.application.config.assets.version = '1.0'
 # Add additional assets to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 Rails.application.config.assets.paths << Rails.root.join('node_modules/bootstrap-icons/font')
-
-# Precompile additional assets.
-Rails.application.config.assets.precompile += %w(bootstrap-icons.woff2 bootstrap-icons.woff)
