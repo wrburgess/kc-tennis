abb97c326037a5925a41939c4f9606cf7d4b51c8
Randy Burgess <wrburgess@gmail.com>
2024-12-10 22:17:48 -0600

    correct plural of controller name

--- app/controllers/admin/inbound_request_logs_controller.rb
+++ app/controllers/admin/inbound_request_logs_controller.rb
@@ -1,4 +1,4 @@
-class Admin::InboundRequestLogController < ApplicationController
+class Admin::InboundRequestLogsController < ApplicationController
   include Pagy::Backend
 
   before_action :authenticate_user!
