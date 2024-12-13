d04a6d5688899ddc24537c7bc02612e703e6690d
Randy Burgess <wrburgess@gmail.com>
2024-12-13 16:50:07 -0600

    updating dependencies

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,5 +16,28 @@ admins = [
 admins.each do |admin|
   User.create_with(admin).find_or_create_by(email: admin[:email])
 end
-
 puts "END:   Create admins, Users Count: #{User.count}"
+
+# CREATE Admins
+puts "BEGIN: Create users"
+admins = [
+  { email: "wrburgess@gmail.com", first_name: "Randy", last_name: "Burgess", password: "dtf6fhu7pdq6nbz-RED", confirmed_at: Time.now.utc }
+]
+[1..30].each do |admin|
+  User.create(
+    email: Faker::Internet.email,
+    first_name: Faker::Person.first_name,
+    last_name: Faker::Person.last_name,
+    password: Faker::Internet.password,
+    confirmed_at: Time.now.utc
+  )
+end
+puts "END:   Create users, Users Count: #{User.count}"
+
+# CREATE Links
+puts "BEGIN: Create Links"
+
+[1..30].each do
+  Link.create(url: Faker::Internet.url, url_type: UrlTypes.all.sample, secure_code: SecureRandom.hex(10), notes: Faker::Lorem.sentence)
+end
+puts "END:   Create Links, Links Count: #{Link.count}"
