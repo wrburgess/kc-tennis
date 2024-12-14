0d6dda38a369f116495245226c1d623e3f5dee1f
Randy Burgess <wrburgess@gmail.com>
2024-12-13 19:47:27 -0600

    additional admin comp changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,4 +12,12 @@ module ApplicationHelper
       date_value
     end
   end
+
+  def selector_date_format(date_value)
+    if date_value.respond_to?(:strftime)
+      date_value.strftime('%Y-%m-%d')
+    else
+      date_value
+    end
+  end
 end
